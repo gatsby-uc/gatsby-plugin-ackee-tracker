05b79b75195df698323aabeacf29cc2e4d06de92
Christopher Burns <christopher@tapkit.io>
2019-10-20 19:18:28 +0100

    chnaged vars

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,10 +1,10 @@
 import ackeeTracker from 'ackee-tracker';
 
 exports.onRouteUpdate = ({ location }) => {
-  const domainId = window.GATSBY_ACKEE_DOMAIN_ID;
-  const server = window.GATSBY_ACKEE_SERVER;
-  const ignoreLocalhost = window.GATSBY_ACKEE_IGNORE_LOCALHOST;
-  const detailed = window.GATSBY_ACKEE_DETAILED;
+  const domainId = window.GATSBY_PLUGIN_ACKEE_DOMAIN_ID;
+  const server = window.GATSBY_PLUGIN_ACKEE_SERVER;
+  const ignoreLocalhost = window.GATSBY_PLUGIN_ACKEE_IGNORE_LOCALHOST;
+  const detailed = window.GATSBY_PLUGIN_ACKEE_DETAILED;
 
   const instance = ackeeTracker.create({
     server: server,
