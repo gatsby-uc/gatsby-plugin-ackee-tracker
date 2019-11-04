c307f174c96179bb4825701201529d3ccf2607ac
Christopher Burns <christopher@tapkit.io>
2019-11-04 08:48:55 +0000

    fixed broken tracking

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,38 +1,67 @@
 import * as ackeeTracker from 'ackee-tracker';
 
+let trackerInstance = null;
+
 /**
- * Gatsby Plugin Ackee
+ * onInitialClientRender
  *
- * A Gatsby Plugin for usingP ackee tracker
+ * Adds Ackee Tracker to website when it renders page
  *
- * @export onRouteUpdate
- * @param {*} {
- *   location,
- *   prevLocation
- * }
- * @param {*} {
- *   domainId,
- *   server,
- *   ignoreLocalhost,
- *   detailed,
- * }
+ * @export onInitialClientRender
+ * @param {*} { domainId, server, ignoreLocalhost, detailed }
+ * @param boolean record instance
  */
-export function onRouteUpdate(
+const AckeeTraker = (
+  { domainId, server, ignoreLocalhost, detailed },
+  record
+) => {
+  // checks if domainID and Server is present and also if tracker is null then create tracking instance
+  if (domainId && server && trackerInstance === null) {
+    trackerInstance = ackeeTracker.create(
+      {
+        server,
+        domainId,
+      },
+      {
+        ignoreLocalhost,
+        detailed,
+      }
+    );
+
+    //Tracks initial Page
+    trackerInstance.record();
+  }
+  // checks if record is true and that the tracker instance exists
+  if (record && trackerInstance) {
+    // records update on new pages
+    trackerInstance.record();
+  }
+};
+
+/**
+ * onInitialClientRender
+ *
+ * Adds Ackee Tracker to website when it renders page
+ *
+ * @export onInitialClientRender
+ * @param {*} _
+ * @param {*} { domainId, server, ignoreLocalhost, detailed }
+ */
+export function onInitialClientRender(
   _,
   { domainId, server, ignoreLocalhost, detailed }
 ) {
-  // Create Instance of the Ackee Tracker
-  const instance = ackeeTracker.create(
-    {
-      server: server,
-      domainId: domainId,
-    },
-    {
-      ignoreLocalhost: ignoreLocalhost,
-      detailed: detailed,
-    }
-  );
+  AckeeTraker({ domainId, server, ignoreLocalhost, detailed }, false);
+}
 
+/**
+ * onRouteUpdate
+ *
+ * Added function call to where an instrance of the tracker is and calls a update of that tracker
+ *
+ * @export onRouteUpdate
+ */
+export function onRouteUpdate() {
   // record attributes
-  instance.record();
+  AckeeTraker(false, true);
 }
